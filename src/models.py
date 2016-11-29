9145c204548649aa2b5720f863ee0709b7f60a1a
Lijia Xie <lijiaxie@college.harvard.edu>
2016-11-29 02:04:51 -0500

    added random pathing

--- src/models.py
+++ src/models.py
@@ -1,10 +1,12 @@
 import sqlite3
 import numpy as np
+import random
+
 
 class Graph:
-    def __init__(self, binary, db_path, dbg = False):
+    def __init__(self, binary, db_path, dbg=False):
         if dbg:
-        #     TODO: initialize logging
+            # TODO: initialize logging
             print "Loading file"
 
         self.header_size = 4
@@ -13,8 +15,8 @@ class Graph:
         self.db = sqlite3.connect(db_path)
         self.cur = self.db.cursor()
 
-    def at(self, pos, offset):
-        return self.array[pos / 4 + offset]
+    def at(self, pos, int_offset):
+        return self.array[pos / 4 + int_offset]
 
     def link_count(self, pos):
         return self.at(pos, 1)
@@ -49,8 +51,33 @@ class Graph:
 
     def name(self, pos):
         self.cur.execute("SELECT title FROM pages WHERE offset =:offset LIMIT 1", {"offset": int(pos)})
-        return self.cur.fetchone()
+        return self.cur.fetchone()[0]
 
     def find(self, string):
         self.cur.execute("SELECT offset FROM pages WHERE title =:title LIMIT 1", {"title": string})
-        return self.cur.fetchone()
+        return self.cur.fetchone()[0]
+
+    def to_matrix(self):
+        pass
+
+    def get_path(self, length):
+        start_int = random.randint(0, len(self.array))
+        while self.array[start_int] != 0:
+            start_int += 1
+            if start_int >= len(self.array):
+                start_int = 0
+
+        page_links = self.page_links(start_int * 4)
+        seed = random.sample(page_links, 1)[0]
+
+        path = [seed]
+        cur_node = seed
+        nodes = len(path)
+        while nodes != length:
+            links = self.page_links(cur_node)
+            next_node = random.sample(links, 1)[0]
+            path.append(next_node)
+            cur_node = next_node
+            nodes += 1
+
+        return path
